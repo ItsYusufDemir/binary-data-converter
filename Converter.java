bec076ae74475b99747d50afa57606c4d6ab9960
Yusuf Demir <jamesyusuf36@gmail.com>
2023-04-12 00:17:27 +0300

    Starting

--- Converter.java
+++ Converter.java
@@ -6,6 +6,27 @@
 
 public class Converter {
 
+    //GLOBAL VARIABLES
+    int dataType;  //1: unsigned int, 2: signed int, 3: floating number
+    int sizeOfData; // 1, 2, 3 or 4 bytes.
+    boolean isLittleEndian;
+
+
+    public static void main(String args[]){
+
+        takeInputs();  //It will ask for the data type, data size and byte ordering from the user and update the global variables
+
+
+
+
+
+
+
+
+
+
+    }
+
 
 
 }
